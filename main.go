c016934f668ccd6677de14e2b6854831906f09f6
Charles N <charlesnowow@gmail.com>
2018-03-29 21:50:42 +0100

    graceful exit

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
@@ -11,6 +14,29 @@ func init() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	gin.SetMode(gin.ReleaseMode)
 }
+
+func gracefulExit(server *http.Server) {
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	go func() {
+		<-quit
+		log.Println("receive interrupt signal")
+		if err := server.Close(); err != nil {
+			log.Fatal("Server Close:", err)
+		}
+	}()
+}
+
+func runServer(server *http.Server) {
+	if err := server.ListenAndServe(); err != nil {
+		if err == http.ErrServerClosed {
+			log.Println("Server closed under request")
+		} else {
+			log.Fatal("Server closed unexpect")
+		}
+	}
+}
+
 func main() {
 	r := gin.Default()
 
@@ -21,6 +47,10 @@ func main() {
 		c.String(200, "pong")
 	})
 
-	log.Println("http://localhost:8080/")
-	r.Run(":8080")
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	gracefulExit(server)
+	runServer(server)
 }
