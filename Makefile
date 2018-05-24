198a564bc4ca0f56e821408e2567f9e3e5ae9fb5
Charles N <charlesnowow@gmail.com>
2018-05-24 23:18:44 +0100

    graceful exit ok

--- Makefile
+++ Makefile
@@ -1,4 +1,20 @@
-.PHONY: run
+.SILENT:
+.ONESHELL:
+.NOTPARALLEL:
+.EXPORT_ALL_VARIABLES:
+.PHONY: run deps build clean exec
 
-run:
-	go run *.go
+run: build exec clean
+
+exec:
+	./bin/app
+
+build:
+	go build -o bin/app -ldflags '-s -w -extldflags "-static"'
+
+clean:
+	rm -rf bin
+
+deps:
+	go get -d -u -v ./...
+
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 	"os/signal"
 	"runtime"
@@ -17,47 +16,20 @@ func init() {
 	if runtime.GOOS == "windows" {
 		gin.DisableConsoleColor()
 	}
-}
-
-func gracefulExit(server *http.Server) {
 	quit := make(chan os.Signal)
 	signal.Notify(quit, os.Interrupt)
 	go func() {
 		<-quit
-		log.Println("receive interrupt signal")
-		if err := server.Close(); err != nil {
-			log.Fatal("Server Close:", err)
-		}
+		log.Println("KEKK")
 		os.Exit(0)
 	}()
 }
 
-func runServer(server *http.Server) {
-	if err := server.ListenAndServe(); err != nil {
-		if err == http.ErrServerClosed {
-			log.Println("Server closed under request")
-		} else {
-			log.Fatal("Server closed unexpect")
-		}
-	}
-}
-
 func main() {
-
 	r := gin.Default()
 
-	r.Use(gin.Recovery())
 	r.Use(static.Serve("/", static.LocalFile("./public", true)))
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
-
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: r,
-	}
-	log.Println("http://localhost:8080")
-	gracefulExit(server)
-	runServer(server)
+	log.Println("running on http://localhost:8080")
+	r.Run(":8080")
 }
